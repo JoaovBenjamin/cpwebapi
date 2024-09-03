e0bd1efb42af015756e0421e87988141f5bae365
Joaovvb <rm98938@fiap.com.br>
2024-09-02 23:38:18 -0300

    doc

--- webapi/Controllers/ExchangeController.cs
+++ webapi/Controllers/ExchangeController.cs
@@ -32,19 +32,14 @@ namespace webapi.Controllers
 {
         try
     {
-        // Obtendo a resposta da API externa como uma string
         var jsonResponse = await _externalApiService.GetAsync();
 
-        // Parsing da string JSON para um objeto JObject
         var jsonObject = JObject.Parse(jsonResponse);
 
-        // Extraindo o valor da propriedade "conversion_rate" diretamente como string
         var conversionRateString = jsonObject["conversion_rate"].ToString();
         
-        // Verificando se "conversionRateString" é um número (float ou double)
         if (double.TryParse(conversionRateString, out double conversionRate))
         {
-            // Criando o resultado com apenas a taxa de conversão
             var result = new JsonResult(new { ConversionRate = conversionRate });
             return result;
         }
@@ -55,7 +50,7 @@ namespace webapi.Controllers
     }
     catch (Exception ex)
     {
-        return new JsonResult(new { Error = ex.Message }) { StatusCode = 400 }; // Definindo o StatusCode para 400 (BadRequest)
+        return new JsonResult(new { Error = ex.Message }) { StatusCode = 400 }; 
     }
     
     }
