8b3939562f301116f3caff66871aafe6ba256eec
Joaovvb <rm98938@fiap.com.br>
2024-09-02 23:37:16 -0300

    Conversion Rate

--- webapi/Controllers/ExchangeController.cs
+++ webapi/Controllers/ExchangeController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using service;
 using webapi.models;
 using Swashbuckle.AspNetCore;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
 
 namespace webapi.Controllers
 {
@@ -28,16 +30,34 @@ namespace webapi.Controllers
         [HttpGet("GetExchangeRate")]
       public async Task<JsonResult> GetExchangeRate()
 {
-    try
+        try
     {
-        var rate = await _externalApiService.GetAsync();
-        var result = new JsonResult(new { conversion_rate = rate });
-        return result; // Retornando diretamente o JsonResult
+        // Obtendo a resposta da API externa como uma string
+        var jsonResponse = await _externalApiService.GetAsync();
+
+        // Parsing da string JSON para um objeto JObject
+        var jsonObject = JObject.Parse(jsonResponse);
+
+        // Extraindo o valor da propriedade "conversion_rate" diretamente como string
+        var conversionRateString = jsonObject["conversion_rate"].ToString();
+        
+        // Verificando se "conversionRateString" é um número (float ou double)
+        if (double.TryParse(conversionRateString, out double conversionRate))
+        {
+            // Criando o resultado com apenas a taxa de conversão
+            var result = new JsonResult(new { ConversionRate = conversionRate });
+            return result;
+        }
+        else
+        {
+            throw new Exception("O valor de 'conversion_rate' não é um número válido.");
+        }
     }
     catch (Exception ex)
     {
         return new JsonResult(new { Error = ex.Message }) { StatusCode = 400 }; // Definindo o StatusCode para 400 (BadRequest)
     }
-}
+    
     }
 }
+    }
